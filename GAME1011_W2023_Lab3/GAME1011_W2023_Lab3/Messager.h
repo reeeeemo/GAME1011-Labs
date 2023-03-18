5ad2ab565c7853a058c0675328e9ba0e008d0074
reeeeemo <robbie@oxley.ca>
2023-03-18 15:09:26 -0400

    [MOD] Final changes

--- GAME1011_W2023_Lab3/GAME1011_W2023_Lab3/Messager.h
+++ GAME1011_W2023_Lab3/GAME1011_W2023_Lab3/Messager.h
@@ -13,40 +13,12 @@ public:
 	virtual ~Messager() = 0;
 
 
-	// Functions to call and send a vector.
-	static void WelcomePlayer();
-
-	template <typename DataType>
-	static UnorderedArray<DataType> AskForListInput();
-
-
 	/* Outputting whatever list of data types it is given. */
 	template <typename DataType>
 	static void OutputList(UnorderedArray<DataType> &output_array);
 
 private:
-	/* Recursive function that asks forand grabs input of player. */
-	template <typename DataType>
-	static void GetListInput(int numOfInputs)
-	{
-		if (numOfInputs <= 0) {
-			// Break out of recursion
-		}
-		else {
-			DataType n;
-			std::cin >> n;
-
-			input.push_back(n);
-			GetListInput(numOfInputs - 1);
-		}
-	}
-
-	template <typename DataType>
-	static UnorderedArray<DataType> input;
 };
-
-template <typename DataType>
-UnorderedArray<DataType> Messager::input;
 
 template <typename DataType>
 void Messager::OutputList(UnorderedArray<DataType> &output_array)
@@ -60,29 +32,5 @@ void Messager::OutputList(UnorderedArray<DataType> &output_array)
 		}
 	}
 }
-
-
-
-/* Asks for list inputand calls recursive function based on how many data types is being entered. */
-template <typename DataType>
-UnorderedArray<DataType> Messager::AskForListInput()
-{
-	int numOfTypes;
-	std::cout << "Please input the number of data types you will be entering!\n";
-	std::cin >> numOfTypes;
-
-	input = UnorderedArray<DataType>(numOfTypes);
-
-	std::cout << "Please input the data types now:\n\n";
-
-	GetListInput(numOfTypes);
-
-	return input;
-}
-
-
-
-
-
 
 #endif //!__MESSAGER_H__
--- GAME1011_W2023_Lab3/GAME1011_W2023_Lab3/main.cpp
+++ GAME1011_W2023_Lab3/GAME1011_W2023_Lab3/main.cpp
@@ -7,14 +7,27 @@
 #include <string>
 
 int main() {
-	Messager::WelcomePlayer();
 
-	UnorderedArray<int> float_array = Messager::AskForListInput<int>();
+	// Array of floats 
+	UnorderedArray<float> float_array = UnorderedArray<float>(3);
 	
-	
+	float_array.push_back(3.5f);
+	float_array.push_back(1.2f);
+	float_array.push_back(7.8f);
+
+
+	std::cout << "Array of floats: \n";
 	Messager::OutputList(float_array);
+
+	std::cout << "\nFinding value '3.5' via Linear search: \n";
+	std::cout << float_array.SearchForValue(3.5f, SearchType::LINEAR) << std::endl;
+
+	std::cout << "\nFinding value '1.2' via Binary search: \n";
+	std::cout << float_array.SearchForValue(1.2f, SearchType::BINARY) << std::endl;
 	
-	std::cout << float_array.SearchForValue(6, SearchType::BINARY);
+
+	std::cout << "\nSorted array of floats: \n";
+	Messager::OutputList(float_array);
 
 	float_array.clear();
 	return 0;
